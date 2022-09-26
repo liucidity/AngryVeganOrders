fcbd3b9674adf7aa0f5b08c73a0a9b89766ca6a8
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-26 16:48:25 -0600

    separated preorder html to its own component (js) for future views manager implementations

--- public/scripts/components/preorder.js
+++ public/scripts/components/preorder.js
@@ -42,4 +42,44 @@ $(() => {
 </body>
 `
   );
+  window.$preorder = $preorder;
+
+  //////////////////trello just form for cellphone input//////////////////////////
+  const phoneInputField = document.querySelector("#phone");
+  const phoneInput = window.intlTelInput(phoneInputField, {
+    initialCountry: "ca",
+    preferredCountries: ["ca", "mx"],
+    geoIpLookup: getIp,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+  });
+  const info = document.querySelector(".alert-info");
+
+  $("#numberInput").on("submit", function (event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    $.ajax({
+      method: "POST",
+      url: "/api/users",
+      data,
+    }).then(console.log("this works"));
+  });
+
+  function getIp(callback) {
+    fetch("https://ipinfo.io/json?token=<your token>", {
+      headers: { Accept: "application/json" },
+    })
+      .then((resp) => resp.json())
+      .catch(() => {
+        return {
+          country: "us",
+        };
+      })
+      .then((resp) => callback(resp.country));
+  }
+
+  // getAllListings(data).then(function( json ) {
+  //   propertyListings.addProperties(json.properties);
+  //   views_manager.show('preorders');
+  //   a}
 });

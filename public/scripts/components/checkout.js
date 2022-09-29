0c14dcac2be705f64cbe7a256c708937f34ba264
hugo-pb <h.palomerabecerra@gmail.com>
2022-09-28 19:49:10 -0600

    ckeckout page  is working / added visuals for product

--- public/scripts/components/checkout.js
+++ public/scripts/components/checkout.js
@@ -1,43 +1,36 @@
 $(() => {
   window.createCheckout = {};
-  const createCheckouts = (item) => {
-    const $check = $(
-      `
-      <nav class="navbar navbar-light bg-light">
-        <button class="btn"><i class="fa-solid fa-arrow-left"></i> Back</button>
-        <a class="navbar-brand" href="#">
-          <img
-            src="https://www.angryveganpenticton.ca/app/uploads/Angry-Vegan.svg"
-            width="30"
-            height="30"
-            class="d-inline-block align-top"
-            alt=""
-          />
-          Angry Vegan
-        </a>
-        <div></div>
-      </nav>
-      <header class="m-5">
-        <h2 class="text-center">Pickup</h2>
-      </header>
-      <div class="card text-center">
-        <div class="card-header">Your items</div>
+  const createCheckouts = (items) => {
+    const $list = $("<div></div>");
+    for (const item of items) {
+      const $check = $(
+        // <nav class="navbar navbar-light bg-light">
+        //   <button class="btn"><i class="fa-solid fa-arrow-left"></i> Back</button>
+        //   <a class="navbar-brand" href="#">
+        //     <img
+        //       src="https://www.angryveganpenticton.ca/app/uploads/Angry-Vegan.svg"
+        //       width="30"
+        //       height="30"
+        //       class="d-inline-block align-top"
+        //       alt=""
+        //     />
+        //     Angry Vegan
+        //   </a>
+        //   <div></div>
+        // </nav>
+        //   <header class="m-5">
+        //   <h2 class="text-center">Pickup</h2>
+        // </header>
+        //<div class="card-header">Your items</div>
+        `<div class="card text-center">
         <div class="card-body">
           <div class="container" id="">
             <div class="menu-item card mb-3 row">
               <div class="row g-0">
                 <!-- image container -->
-                <div class="col-3">
-                  <img
-                    src="${item.picture_url}"
-                    class="img-fluid rounded-start"
-                    alt="burger"
-                    width="128"
-                    height="128"
-                  />
-                </div>
+
                 <!-- card right section -->
-                <div class="col-9">
+                <div class="col-12">
                   <!-- card body -->
                   <div class="card-body row">
                     <h5 class="card-title">${item.name}</h5>
@@ -45,45 +38,49 @@ $(() => {
                     <!-- price, cart and quantity -->
                     <div class="cart-functions d-flex justify-content-between">
                       <div>
-                        <div class="item-price">${item.subtotal}</div>
+                        <div class="item-price">${item.item_totals}</div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div class="card">
-            <p>subtotal: ${item.subtotal}</p>
-            <br />
-            <p>tax: ${item.subtotal * 0.12}</p>
-            <br />
-            <p>TOTAL: ${
-              Number(item.subtotal) + Number(item.subtotal * 0.12)
-            }</p>
-          </div>
-          <button type="submit" id='orderConfirmation' class="btn btn-success w-50 mt-2 mb-3">
-            confirm order
-          </button>
-          <button type="submit" class="btn btn-danger mt-2 mb-3">
-            Edit cart
-          </button>
-          <div class="card-footer text-muted">Angry Vegan</div>
-        </div>
+          </div>`
+      );
 
-    <script
-      type="text/javascript"
-      src="./scripts/components/checkout.js"
-    ></script>
-      </div>
-  `
-    );
-    return $check;
+      $check.appendTo($list);
+    }
+    const $final = $(` <div class="card">
+          <p>subtotal: ${items[0].subtotal}</p>
+          <br />
+          <p>tax: ${items[0].subtotal * 0.12}</p>
+          <br />
+          <p>TOTAL: ${
+            Number(items[0].subtotal) + Number(items[0].subtotal * 0.12)
+          }</p>
+              </div>
+              <button type="submit" id='orderConfirmation' class="btn btn-success w-50 mt-2 mb-3">
+                confirm order
+              </button>
+              <button type="submit" class="btn btn-danger mt-2 mb-3">
+                Edit cart
+              </button>
+              <div class="card-footer text-muted">Angry Vegan</div>
+            </div>
+
+        <script
+          type="text/javascript"
+          src="./scripts/components/checkout.js"
+        ></script>
+          </div>`);
+
+    $final.appendTo($list);
+    return $list;
   };
   window.createCheckout.createCheckout = createCheckouts;
 
   /////////////////rendering page Window_manager////////////////////////////////////////
-  const $checkout = $("<div></div>");
+  const $checkout = $(`<div></div>`);
 
   window.$checkout = $checkout;
 
@@ -102,7 +99,7 @@ $(() => {
 
     $.get(`/api/carts/${cartData}`, (data) => {
       console.log("data", data);
-      const checkoutElement = window.createCheckout.createCheckout(data[0]);
+      const checkoutElement = window.createCheckout.createCheckout(data);
       appendCheckoutElement(checkoutElement);
     });
 
